440ee48a841245e638e8d307030318a6f20922b9
alicia chaing <aliciawchiang@gmail.com>
2022-07-15 16:25:48 -0700

    iteration 0 complete

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+class ToDo{
+    var name = "broken"
+    var important = true
+    
+//    init(name1: String, important1: Bool){
+//        name = name1
+//        important = important1
+//    }
+}
